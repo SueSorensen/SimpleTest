a9f0b9bb928112cb013567c1548aebcb7acb7825
SueSorensen <theothercanadian@msn.com>
2022-04-05 15:37:19 -0700

    SimpleTest initial version

---
+++ Data/NorthwindCategories.cs
@@ -0,0 +1,9 @@
+//namespace SimpleTest.Data.Northwind; // Razor won't recognize third level namespace
+namespace SimpleTest.Northwind;
+
+public class CategoriesType
+{
+    public double CategoryID { get; set; }
+    public string Description { get; set; }
+    public string Name { get; set; }
+}
